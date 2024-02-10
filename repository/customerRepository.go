1067c32b4bbb122f2074b1540e55a9e5b54dd1b8
brenonaraujo <brenonaraujo@gmail.com>
2024-02-10 20:12:52 -0300

    feat: Remove unused reposioty at this first implementation version

--- repository/customerRepository.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-type CustomerRepository interface {
-	DeductBalance(customerId string, amount float64) error
-	AddBalance(customerId string, amount float64) error
-	CreateTransaction(customerId string, amount float64, transactionType string, description string) error
-	GetBalance(customerId string) (float64, error)
-}
